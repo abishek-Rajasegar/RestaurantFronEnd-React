115cb528e2f582a22819ed376461e90faa79e1f4
abishek-Rajasegar <64126159+abishek-Rajasegar@users.noreply.github.com>
2021-01-07 16:20:24 +0530

    Add files via upload

---
+++ abishek_rajasegar3-confu-sion-ReactJs/abishek_rajasegar3-confu-sion-9af699130f4a/json-server/public/precache-manifest.ebab000757c21cb71ace061781c4f5e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dc1a2affe291891e9a272c4766b363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797fc9ca32084a407790",
+    "url": "/static/css/2.7d9277a0.chunk.css"
+  },
+  {
+    "revision": "b9262e751ed78422d0ff",
+    "url": "/static/css/main.ac120bab.chunk.css"
+  },
+  {
+    "revision": "797fc9ca32084a407790",
+    "url": "/static/js/2.2b881e19.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2b881e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9262e751ed78422d0ff",
+    "url": "/static/js/main.ff55b3b8.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
